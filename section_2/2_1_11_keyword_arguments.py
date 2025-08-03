a72525f9fc86fa33f4634845cf11631f133f906a
Shokhijakhon Kodirov <skodirov@slb.com>
2025-08-03 09:23:08 +0500

    python initial lesson

---
+++ section_2/2_1_11_keyword_arguments.py
@@ -0,0 +1,25 @@
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+
+
+
+# print("My name is ", end="")
+# print("Monty Python.")
+
+
+
+# print("My", "name", "is", "Monty", "Python.", sep="-")
+
+
+
+# print("My", "name", "is", "Monty", "Python.", sep="-")
+
+
+
+
+# print("My", "name", "is", sep="_", end="*")
+# print("Monty", "Python.", sep="*", end="*\n")
+
+
+
---
+++ section_2/2_1_1_hello_world.py
@@ -0,0 +1,13 @@
+print("Hello, world!")
+
+
+"""
+    As you can see, this first program consists of the following parts:
+    - the word print;
+    - an opening parenthesis;
+    - a quotation mark;
+    - a line of text: Hello, World!;
+    - another quotation mark;
+    - a closing parenthesis.
+    Each of the above plays a very important role in the code.
+"""
